55a6f5fba6aa9d5b43e5b7ca2cbb7938a5698df5
pdkpro <phuc.pham.itf@gmail.com>
2019-08-17 15:09:31 +0700

    Add user the link to edit a share movide authorize.

--- app/helpers/shares_helper.rb
+++ app/helpers/shares_helper.rb
@@ -2,6 +2,10 @@ module SharesHelper
   def youtube_video(youtube_url, css_class = 'embed-responsive-item')
     youtube_params = youtube_url.split("?").second
     youtube_id = youtube_params.split("&").first.split('=').last
-    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", class: css_class)
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", class: css_class, alt: 'Video loading . . .!')
+  end
+
+  def editable?(share)
+    current_user && current_user.id == share.user_id
   end
 end
